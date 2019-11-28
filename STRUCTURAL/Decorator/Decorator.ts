8e6ffe37f2218fc4d47192665803877de6bac10b
artem.diashkin <artem.diashkin@litslink.com>
2019-11-28 18:16:46 +0200

    c

--- STRUCTURAL/Decorator/Decorator.ts
+++ STRUCTURAL/Decorator/Decorator.ts
@@ -14,7 +14,7 @@ class Decorator implements IComponent {
 
     public operation(): string {
         return this.component.operation();
-        //asdasdasd
+        //asdasdasd asd asd
 
     }
 }
